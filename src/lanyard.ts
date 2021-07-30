27e755f7e9da05edd15a7187bb2cea1a4343be6b
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-07-30 14:50:20 -0400

    feat: re-connecting websocket

--- src/lanyard.ts
+++ src/lanyard.ts
@@ -3,11 +3,12 @@ import {
 	LanyardData,
 	LanyardOptions,
 	LanyardResponse,
+	LanyardSWRMultiple,
 	LanyardSWRSingle,
 	LanyardWebsocket,
-	LanyardSWRMultiple,
 } from "./types";
 import { useEffect, useState } from "react";
+
 import useSWR from "swr";
 
 export const useLanyard = (
@@ -26,44 +27,53 @@ export const useLanyard = (
 					"Browser doesn't support WebSocket connections.",
 				);
 
-			const socket = new WebSocket(WEBSOCKET_URL);
-			setWebsocket(socket);
-
 			let key = "subscribe_to_id";
 			if (typeof options.userId === "object") key = "subscribe_to_ids";
 
 			let heartbeat: NodeJS.Timeout;
+			let socket: WebSocket;
 
-			socket.addEventListener("open", () => {
-				socket.send(
-					JSON.stringify({
-						op: 2,
-						d: {
-							[key]: options.userId,
-						},
-					}),
-				);
+			const connectWebsocket = () => {
+				socket = new WebSocket(WEBSOCKET_URL);
+				setWebsocket(socket);
+				setLoading(true);
 
-				heartbeat = setInterval(() => {
+				socket.addEventListener("open", () => {
 					socket.send(
 						JSON.stringify({
-							op: 3,
+							op: 2,
+							d: {
+								[key]: options.userId,
+							},
 						}),
 					);
-				}, 30000);
-			});
 
-			socket.addEventListener("message", ({ data }) => {
-				const { t, d } = JSON.parse(data) as {
-					t: "INIT_STATE" | "PRESENCE_UPDATE";
-					d: LanyardData;
-				};
+					heartbeat = setInterval(() => {
+						socket.send(
+							JSON.stringify({
+								op: 3,
+							}),
+						);
+					}, 30000);
+				});
 
-				if (t === "INIT_STATE" || t === "PRESENCE_UPDATE") {
-					setStatus(d || ({} as LanyardData));
-					if (loading) setLoading(false);
-				}
-			});
+				socket.addEventListener("message", ({ data }) => {
+					const { t, d } = JSON.parse(data) as {
+						t: "INIT_STATE" | "PRESENCE_UPDATE";
+						d: LanyardData;
+					};
+					if (t === "INIT_STATE" || t === "PRESENCE_UPDATE") {
+						setStatus(d || ({} as LanyardData));
+						if (loading) setLoading(false);
+					}
+				});
+
+				socket.addEventListener("close", () => {
+					connectWebsocket();
+				});
+			};
+
+			connectWebsocket();
 
 			return () => {
 				clearInterval(heartbeat);
