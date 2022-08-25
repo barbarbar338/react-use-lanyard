1204bc513b8498edf5eb0b4edc60bf6169bfdf1b
Barış DEMİRCİ <demirci.baris38@gmail.com>
2022-08-25 14:11:24 +0000

    🚑 Update API url (publish)

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
-export const API_URL = "https://api.lanyard.rest/v1";
-export const WEBSOCKET_URL = "wss://api.lanyard.rest/socket";
+export const API_URL = "https://lanyard.rest/v1";
+export const WEBSOCKET_URL = "wss://lanyard.rest/socket";
 export const HEARTBEAT_INTERVAL = 30_000;
